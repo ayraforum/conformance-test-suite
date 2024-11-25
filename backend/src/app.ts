e1470613ace63d637f1df4b8f617561ab04de997
David Poltorak <dave.poltorak@gmail.com>
2024-11-25 17:10:42 +0000

    feat: added Swagger gen to backend
    Signed-off-by: David Poltorak <dave.poltorak@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
 import cors from "cors";
 import { Server } from "socket.io";
+import swaggerUi from 'swagger-ui-express';
+import { openApiDocument } from "./controllers/aggregatorController";
 
 const app = express();
 
@@ -11,5 +13,8 @@ app.use(express.json());
 
 // Error Handling
 app.use(errorHandler);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+
+
 
 export default app;
--- shared/src/testContract.ts
+++ shared/src/testContract.ts
@@ -1,5 +1,8 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
+import { extendZodWithOpenApi } from '@anatine/zod-openapi';
+
+extendZodWithOpenApi(z);
 
 // Initialize the contract
 const c = initContract();
