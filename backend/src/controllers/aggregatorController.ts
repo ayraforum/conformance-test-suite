6fa9f1e0fb7a7cbccc8ffcfa72568cfa38e397f9
David Poltorak <dave.poltorak@gmail.com>
2024-11-25 17:10:42 +0000

    feat: added Swagger gen to backend
    Signed-off-by: David Poltorak <dave.poltorak@gmail.com>

--- backend/src/controllers/aggregatorController.ts
+++ backend/src/controllers/aggregatorController.ts
@@ -2,11 +2,19 @@
 import { initServer } from '@ts-rest/express';
 import { contract } from '@conformance-test-suite/shared/src/contractAggregator';
 import { testController } from './testController';
+import { generateOpenApi } from '@ts-rest/open-api';
 
 const s = initServer();
 
 export const controllerAggregator = s.router(contract, {
     ...testController
 });
+
+export const openApiDocument = generateOpenApi(contract, {
+    info: {
+      title: 'Posts API',
+      version: '1.0.0',
+    },
+  });
 
 export default controllerAggregator;
