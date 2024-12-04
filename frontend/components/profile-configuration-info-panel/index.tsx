b983bf0d5db1784a075016bce7cf35836039a884
David Poltorak <dave.poltorak@gmail.com>
2024-12-04 17:52:51 +0000

    wip: adding in oid tests
    Signed-off-by: David Poltorak <dave.poltorak@gmail.com>

--- frontend/components/profile-configuration-info-panel/index.tsx
+++ frontend/components/profile-configuration-info-panel/index.tsx
@@ -6,7 +6,7 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-import { ProfileConfiguration } from "@conformance-test-suite/shared/src/profileConfigurationContract";
+import { ProfileConfiguration, ProfileConfigurationType } from "@conformance-test-suite/shared/src/profileConfigurationContract";
 
 interface ProfileConfigurationInfoPanelProps {
     profileConfiguration: ProfileConfiguration;
@@ -29,6 +29,22 @@ export function ProfileConfigurationInfoPanel({ profileConfiguration }: ProfileC
                     <h3 className="font-semibold">Version</h3>
                     <p className="text-sm text-muted-foreground">{profileConfiguration.description}</p>
                 </div>
+                {profileConfiguration.type === ProfileConfigurationType.API && (
+                    <>
+                        <div>
+                            <h3 className="font-semibold">Authorization Endpoint</h3>
+                            <p className="text-sm text-muted-foreground">{profileConfiguration.configuration.authorizationEndpoint}</p>
+                        </div>
+                        <div>
+                            <h3 className="font-semibold">Client ID</h3>
+                            <p className="text-sm text-muted-foreground">{profileConfiguration.configuration.clientId}</p>
+                        </div>
+                        <div>
+                            <h3 className="font-semibold">JWKS</h3>
+                            <p className="text-sm text-muted-foreground">{profileConfiguration.configuration.jwks}</p>
+                        </div>
+                    </>
+                )}
                 </div>
             </CardContent>
         </Card>
